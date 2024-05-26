eb0b5d92baa84e9150e28eb36765414db2b7178e
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-26 15:13:39 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "subscription_id" {
+  type        = string
+  description = "The Subscription ID used for Azure resources"
+}
 
+variable "client_id" {
+  type        = string
+  description = "The Client ID for the Azure service principal"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The Client Secret for the Azure service principal"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The Tenant ID for the Azure service principal"
+}
